1c2250b9acc1c0887c8a526fa4441ac347f48fb0
Rick <rithvik.al@gmail.com>
2022-01-16 03:19:39 -0800

    Handle json logic

--- server/src/routes/transcriptions/transcriptions.js
+++ server/src/routes/transcriptions/transcriptions.js
@@ -18,7 +18,7 @@ router.post('/', (req, res) => {
   }
 
   // initialize test variables
-  var index = 1
+  var index = 12
   var timestamp = 'PT3M38.95S'
 
   var transcript_phrase = recognizedPhrases[index]
@@ -26,40 +26,65 @@ router.post('/', (req, res) => {
   for (let i = 0; i < transcript_words.length - 1; i++) {
     isTimestampWithinRange(timestamp, transcript_words[i]['offset'], transcript_words[i+1]['offset'])
   }
-
 });
 
 function isTimestampWithinRange(timestamp, leftOffset, rightOffset) {
-    var _timestamp = parseOffset(timestamp)
-    var _leftOffset = parseOffset(leftOffset)
-    var _rightOffset = parseOffset(rightOffset)
+    var parsedTimestamp = parseOffset(timestamp)
+    var parsedLeftOffset = parseOffset(leftOffset)
+    var parsedRightOffset = parseOffset(rightOffset)
 
-    if (_timestamp.length == 1) _timestamp.unshift('0')
-    if (_leftOffset.length == 1) _leftOffset.unshift('0')
-    if (_rightOffset.length == 1) _rightOffset.unshift('0')
+    if (parsedTimestamp.length == 2) parsedTimestamp.unshift('0')
+    if (parsedLeftOffset.length == 2) parsedLeftOffset.unshift('0')
+    if (parsedRightOffset.length == 2) parsedRightOffset.unshift('0')
 
-    console.log(_timestamp)
-    console.log(_leftOffset)
-    console.log(_rightOffset)
+    parsedTimestamp = parsedTimestamp.map(parseFloat)
+    parsedLeftOffset = parsedLeftOffset.map(parseFloat)
+    parsedRightOffset = parsedRightOffset.map(parseFloat)
 
-    return true;
+    var timeStampDate = new Date(0);
+    var leftOffsetDate = new Date(0);
+    var rightOffsetDate = new Date(0);
+
+    timeStampDate.setMinutes(parsedTimestamp[0], parsedTimestamp[1], parsedTimestamp[2])
+    leftOffsetDate.setMinutes(parsedLeftOffset[0], parsedLeftOffset[1], parsedLeftOffset[2])
+    rightOffsetDate.setMinutes(parsedRightOffset[0], parsedRightOffset[1], parsedRightOffset[2])
+
+    // console.log(leftOffsetDate, timeStampDate, rightOffsetDate)
+
+    // if (leftOffsetDate <= timeStampDate && timeStampDate <= rightOffsetDate) {
+    //   console.log('YAY!')
+    // } else {
+    //   console.log('...')
+    // }
+
+    let url = 'https://spsvcprodusw.blob.core.windows.net/bestor-338cca94-e7c5-4fc1-8a7b-7b8c19fd291a/TranscriptionData/8fb95f81-a41c-435c-9f2d-4ca06c1fc370_0_0.json?sv=2020-08-04&st=2022-01-15T23%3A14%3A58Z&se=2022-01-16T11%3A19%3A58Z&sr=b&sp=rl&sig=8EEpR2IGxdj%2BzJXGRVBac7pAgOi3Uy8rWt9thPjLT0g%3D';
+    fetch(url)
+      .then(res => res.json())
+      .then(out =>
+        console.log('Checkout this JSON! ', out))
+
+    
+
+    return leftOffsetDate.getTime() <= timeStampDate.getTime() && timeStampDate.getTime() <= rightOffsetDate.getTime();
 };
 
 function parseOffset(timestamp) {
-    var timeArray = timestamp.split(/PT|M|S/)
+    var timeArray = timestamp.split(/PT|M|S|\./)
     timeArray = timeArray.filter(x => x != '');
     return timeArray
 }
 
-//   fetch('https://yesno.wtf/api')
-//     .then(res => res.json())
-//     .then((out) => {
-//       console.log(out);
-//     })
-//     .catch(err => {
-//       throw err
-//     });
-//     return res.status(200); 
-//   });
+
+
+  // fetch('https://yesno.wtf/api')
+  //   .then(res => res.json())
+  //   .then((out) => {
+  //     console.log(out);
+  //   })
+  //   .catch(err => {
+  //     throw err
+  //   });
+  //   return res.status(200); 
+  // });
 
 module.exports = router;
